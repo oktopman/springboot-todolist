59cdb5f99f42be120f99062722965a888e6e0411
oktopman <colaws3@gmail.com>
2020-04-02 22:39:32 +0900

    n+1 문제해결을 위한 어노테이션 추가

--- src/main/java/me/oktop/springboottodolist/domain/todo/TaskRepository.java
+++ src/main/java/me/oktop/springboottodolist/domain/todo/TaskRepository.java
@@ -1,7 +1,12 @@
 package me.oktop.springboottodolist.domain.todo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    @EntityGraph(attributePaths = "comments")
+    Page<Task> findAll(Pageable pageable);
 }
